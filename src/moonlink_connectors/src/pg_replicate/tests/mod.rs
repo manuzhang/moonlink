7932b3e9e1651815b8d5dc09b16e38c5fe705a64
Manu Zhang <OwenZhang1990@gmail.com>
2026-05-22 23:17:59 +0800

    Gate Postgres integration tests behind feature flag

--- src/moonlink_connectors/src/pg_replicate/tests/mod.rs
+++ src/moonlink_connectors/src/pg_replicate/tests/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "postgres-integration")]
 mod cdc_event_test;
+#[cfg(feature = "postgres-integration")]
 mod pg_type_tests;
+#[cfg(feature = "postgres-integration")]
 mod test_utils;
